d5c9d95a471c765db629363150a9d549920c5caf
ogandi <ogandisouw@gmail.com>
2024-11-15 10:40:54 +1100

    port switch

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
 
 const entryRouter = require('./routes/entry-router')
 const geminiRouter = require('./routes/gemini-router')
